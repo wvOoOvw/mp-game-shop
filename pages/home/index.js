2e959b3a5db28470f69d5d20a416f4c556a498a1
xichenchao <xichenchao@shwoody.com>
2026-02-10 13:32:51 +0800

    x

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,82 +1,69 @@
 import Message from 'tdesign-miniprogram/message/index';
-import request from '../../api/request';
+import request from '../../utils/request';
 
-// 获取应用实例
-// const app = getApp()
+const app = getApp()
 
-Page({
-  data: {
-    enable: false,
-    swiperList: [],
-    cardInfo: [],
-    // 发布
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
-  },
-  // 生命周期
-  async onReady() {
-    const [cardRes, swiperRes] = await Promise.all([
-      request('/home/cards').then((res) => res.data),
-      request('/home/swipers').then((res) => res.data),
-    ]);
+const page = {}
 
+page.data = {
+  pullDownEnable: false,
+  cardInfo: [],
+  cardCollectInfo: [],
+  swiperList: [],
+}
+
+page.onLoad = async function (option) {
+  if (option.oper && option.oper === 'release') {
+    this.showOperMsg('发布成功')
+  }
+  if (option.oper && option.oper === 'save') {
+    this.showOperMsg('保存成功')
+  }
+
+  const [cardRes, swiperRes] = await Promise.all([
+    request('/home/cards').then((res) => res.data),
+    request('/home/swipers').then((res) => res.data),
+  ])
+
+  this.setData({
+    cardInfo: cardRes.data,
+    cardCollectInfo: cardRes.data.slice(0, 3),
+    swiperList: swiperRes.data,
+  })
+}
+
+page.onRefresh = async function () {
+  this.setData({
+    pullDownEnable: true,
+  })
+
+  const [cardRes, swiperRes] = await Promise.all([
+    request('/home/cards').then((res) => res.data),
+    request('/home/swipers').then((res) => res.data),
+  ])
+
+  setTimeout(() => {
     this.setData({
+      pullDownEnable: false,
       cardInfo: cardRes.data,
-      focusCardInfo: cardRes.data.slice(0, 3),
       swiperList: swiperRes.data,
-    });
-  },
-  onLoad(option) {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true,
-      });
-    }
-    if (option.oper) {
-      let content = '';
-      if (option.oper === 'release') {
-        content = '发布成功';
-      } else if (option.oper === 'save') {
-        content = '保存成功';
-      }
-      this.showOperMsg(content);
-    }
-  },
-  onRefresh() {
-    this.refresh();
-  },
-  async refresh() {
-    this.setData({
-      enable: true,
-    });
-    const [cardRes, swiperRes] = await Promise.all([
-      request('/home/cards').then((res) => res.data),
-      request('/home/swipers').then((res) => res.data),
-    ]);
+    })
+  }, 1500)
+}
 
-    setTimeout(() => {
-      this.setData({
-        enable: false,
-        cardInfo: cardRes.data,
-        swiperList: swiperRes.data,
-      });
-    }, 1500);
-  },
-  showOperMsg(content) {
-    Message.success({
-      context: this,
-      offset: [120, 32],
-      duration: 4000,
-      content,
-    });
-  },
-  goRelease() {
-    wx.navigateTo({
-      url: '/pages/release/index',
-    });
-  },
-});
+page.onRefresh = function (content) {
+  Message.success({
+    context: this,
+    offset: [120, 32],
+    duration: 4000,
+    content,
+  });
+}
+
+page.onRealease = function () {
+  wx.navigateTo({
+    url: '/pages/release/index',
+  });
+}
+
+Page(page)
--- pages/my/info-edit/index.js
+++ pages/my/info-edit/index.js
@@ -1,4 +1,4 @@
-import request from '../../api/request';
+import request from '../../utils/request';
 import { areaList } from './areaData.js';
 
 Page({
